3e38598aabe2b1ff1a4ed57cbf64349a2623cfe4
Israrali7 <israarali732@gmail.com>
2025-01-25 18:44:36 +0500

    updated code

---
+++ api/index.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const express = require("express")
+const mongoose = require('mongoose');
+const BookRoute = require("../routes/BookRoute");
+const productRoute = require("../routes/productRoute")
+const userAuth = require("../routes/userAuth")
+const uplaodRoute = require('../routes/uploadRoute')
+const cors = require("cors");
+const connectDB = require("../utils/connectDB");
+
+const App = express();
+App.use(express.json());
+// App.use(cors({ origin: "http://localhost:3000"  }));
+App.use(cors());
+
+App.use("/books", BookRoute);
+App.use("/products", productRoute);
+App.use("/auth" , userAuth)
+App.use("/pic" , uplaodRoute)
+
+App.get("/", (req, res) => {
+    res.json({
+        message: "Server is running ok!"
+    })
+})
+
+App.listen(3000, () => {
+    console.log("Server is running");
+    connectDB();
+})
+
+module.exports = App;
